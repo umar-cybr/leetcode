e073304d184586b0803a07ecad0cfe7ce4aa0166
Umar <46139666+umar-cybr@users.noreply.github.com>
2023-07-10 03:37:53 +0000

    Adding logic to the function. Creating a nested for loop that iterates over nth element and ith element. Performs add operation on both then compares with target.

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target:int) -> List[int]:
+        nums = list(input("nums= "))
+        target = input("target+ ")
+        answer = []
+
+        for n in nums:
+            if n[i] + n[i+1] == target:
+                answer = n[i] + n[i+1]
+            else:
+                n += 1
+    twoSum
